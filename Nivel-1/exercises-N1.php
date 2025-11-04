571f57b43d113e93d17765c1ddf39e4cf1809482
Inbisat Naveed <inkyn2002@gmail.com>
2025-11-04 21:05:05 +0100

    Nivel 1 Exercise 2: implement Shape inheritence

--- Nivel-1/exercises-N1.php
+++ Nivel-1/exercises-N1.php
@@ -30,3 +30,57 @@ echo "<br>";
 $Maria = new Employee("Maria", 7500);
 $Maria->calculateTax();  
 ?>
+
+<h1 style="color: darkblue;"> Exercise 1</h1>
+
+<?php
+Class Shape {
+    protected $width;
+    protected $height;
+
+    function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+
+    }
+}
+
+class Triangle extends Shape {
+
+
+    function calculateArea() {
+        $area = ($this->width * $this->height) / 2;
+        return $area;
+
+    }
+
+    function resultArea() {
+        $area = $this->calculateArea();
+        echo "Triangle area  = base: {$this->width}, height: {$this->height} is: " . $area . "<br>";
+    }
+}
+
+
+class Rectangle extends Shape {
+
+
+    function calculateArea() {
+        $area = ($this->width * $this->height);
+        return $area;
+
+    }
+
+    function resultArea() {
+        $area = $this->calculateArea();
+        echo "Rectangle area  = width: {$this->width}, height: {$this->height} is: " . $area . "<br>";
+    }
+}
+
+$triangle = new Triangle(18,13);
+$Rectangle =new Rectangle(12,41);
+
+$triangle->resultArea();
+$Rectangle->resultArea();
+
+
+?>
